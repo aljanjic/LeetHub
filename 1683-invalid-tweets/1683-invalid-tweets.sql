9fc3f502a490f0a38fb26028ce075223a9f0bfdf
Aljosa <janjic_aljosa@hotmail.com>
2024-04-17 18:08:53 +0200

    Solving PostgreSQL, MySQL, Oracl SQL and Pandas

---
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -0,0 +1,19 @@
+-- Write your PostgreSQL query statement below
+Select tweet_id 
+From Tweets 
+Where Length(content) > 15;
+
+-- Write your MySQL query statement below
+Select tweet_id 
+From Tweets
+Where Length(content) > 15; 
+
+/* Write your T-SQL query statement below */
+Select tweet_id 
+From Tweets 
+Where len(content) > 15;
+
+/* Write your PL/SQL query statement below */
+Select tweet_id 
+From Tweets
+Where Length(content) > 15; 

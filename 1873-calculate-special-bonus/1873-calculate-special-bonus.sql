22a4895ac8d59e935fa2e9ed1619c5cd8e6fb64b
Aljosa <janjic_aljosa@hotmail.com>
2024-04-17 18:29:30 +0200

    Solving MySql, PostfreSQL, T-SQL

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,40 @@
+# Write your MySQL query statement below
+Select 
+    employee_id,
+    Case
+        When (employee_id % 2 != 0) And (name not like 'M%') Then salary
+        Else 0
+    End as Bonus
+From Employees
+Order By employee_id asc;
+
+
+-- Write your PostgreSQL query statement below
+Select 
+    employee_id,
+    Case
+        When (employee_id % 2 != 0) And (name not like 'M%') Then salary
+        Else 0
+    End as Bonus
+From Employees
+Order By employee_id asc;
+
+/* Write your T-SQL query statement below */
+Select 
+    employee_id,
+    Case
+        When (employee_id % 2 != 0) And (name not like 'M%') Then salary
+        Else 0
+    End as Bonus
+From Employees
+Order By employee_id asc;
+
+/* Write your PL/SQL query statement below */
+Select 
+    employee_id,
+    Case
+        When (employee_id % 2 != 0) And (name not like 'M%') Then salary
+        Else 0
+    End as Bonus
+From Employees
+Order By employee_id asc;
